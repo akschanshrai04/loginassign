f55ff908f63fde21978b65c9b613968bf9554dbf
akschanshrai04 <akschanshrai97@gmail.com>
2024-12-26 23:15:31 +0530

    hopyhopy

--- backend/middleware/checkauth.js
+++ backend/middleware/checkauth.js
@@ -6,7 +6,10 @@ dotenv.config();
 
 const verifyToken = async (req, res, next) => {
     try {
-        const token = req.cookies["jwt"];
+        // const token = req.cookies["jwt"];
+        const authHeader = req.headers["authorization"];
+        const token = authHeader && authHeader.split(" ")[1];
+        console.log("Authorization header token:", token);
         console.log("inside checkauth : " , token)
         if (!token) {
             return res.status(401).json({ message: "Not authenticated" });
