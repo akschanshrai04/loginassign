689224e62382a4d3275cfabef44b2beba155c068
akschanshrai04 <akschanshrai97@gmail.com>
2024-12-26 22:58:49 +0530

    checkundef

--- backend/middleware/checkauth.js
+++ backend/middleware/checkauth.js
@@ -7,7 +7,7 @@ dotenv.config();
 const verifyToken = async (req, res, next) => {
     try {
         const token = req.cookies["jwt"];
-        console.log(token)
+        console.log("inside checkauth : " , token)
         if (!token) {
             return res.status(401).json({ message: "Not authenticated" });
         }
